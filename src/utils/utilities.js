ea805d448b01caee6e1c02a45442adf50258164b
Nathan Sadala <bernynath@gmail.com>
2021-11-15 15:45:09 +0100

    resolver

--- src/utils/utilities.js
+++ src/utils/utilities.js
@@ -1,23 +1,183 @@
-let processExcel = (exceldata) => {
-    // process excel data and return a js array
-    console.log(exceldata);
+let calculateOutput = (switchData, telco, telcoId, opsType) => {
+    let output = {
+        global_amount_cdf_credit: 0,
+        global_amount_cdf_debit: 0,
+        global_amount_usd_credit: 0,
+        global_amount_usd_debit: 0,
+        checked_transactions: 0,
+        not_matched_transactions: [],
+        not_founded_transactions: []
+    }
+
+    if (telcoId === "1") {
+        computeAirtel(output, switchData, telco, opsType)
+    }
+    else if (telcoId === "2") {
+        computeVodacom(output, switchData, telco, opsType)
+    }
+    else if (telcoId === "3") {
+        computeOrange(output, switchData, telco, opsType)
+    }
+    return output;
 }
 
 
-let processCsv = (csvData) => {
-    // process csv data and return a js array
-    console.log(csvData);
+let computeAirtel = (output, switchData, telco, opsType) => {
+    console.log(output, switchData, telco, opsType)
+}
+
+let computeVodacom = (output, switchData, telco, opsType) => {
+    console.log(switchData, telco, opsType);
+    if (opsType === "1") {
+        switchData.forEach(transactions => {
+            let telco_transac = telco.find(el => el[0] === transactions[0])
+
+            if (telco_transac !== undefined) {
+
+                if (telco_transac[4] === transactions[1] && telco_transac[2] === transactions[4] &&
+                    telco_transac[3] === transactions[2] && telco_transac[1] === transactions[3]) {
+                    if (transactions[1] === "debit") {
+                        if (transactions[4] === "CDF") {
+                            output.global_amount_cdf_debit += parseFloat(transactions[2])
+                            output.checked_transactions += 1
+                        }
+                        else if (transactions[4] === "USD") {
+                            output.global_amount_usd_debit += parseFloat(transactions[2])
+                            output.checked_transactions += 1
+                        }
+                    }
+                    else if (transactions[1] === "credit") {
+                        if (transactions[4] === "CDF") {
+                            output.global_amount_cdf_credit += parseFloat(transactions[2])
+                            output.checked_transactions += 1
+                        }
+                        else if (transactions[4] === "USD") {
+                            output.global_amount_usd_credit += parseFloat(transactions[2])
+                            output.checked_transactions += 1
+                        }
+                    }
+                }
+                else {
+                    output.not_matched_transactions.push(" ")
+                    output.not_matched_transactions.push(transactions)
+                    output.not_matched_transactions.push(telco_transac)
+                }
+            }
+            else{
+                output.not_founded_transactions.push(" ")
+                output.not_founded_transactions.push(transactions)
+            }
+        });
+    }
+    else if (opsType === "2") {
+        telco.forEach(telco_transactions => {
+            let transactions = switchData.find(el => el[0] === telco_transactions[0])
+            if( transactions !== undefined){
+                if (telco_transactions[4] === transactions[1] && telco_transactions[2] === transactions[4] &&
+                    telco_transactions[3] === transactions[2] && telco_transactions[1] === transactions[3]) {
+                    if (telco_transactions[1] === "debit") {
+                        if (telco_transactions[4] === "CDF") {
+                            output.global_amount_cdf_debit += parseFloat(telco_transactions[2])
+                            output.checked_transactions += 1
+                        }
+                        else if (telco_transactions[4] === "USD") {
+                            output.global_amount_usd_debit += parseFloat(telco_transactions[2])
+                            output.checked_transactions += 1
+                        }
+                    }
+                    else if (telco_transactions[1] === "credit") {
+                        if (telco_transactions[4] === "CDF") {
+                            output.global_amount_cdf_credit += parseFloat(telco_transactions[2])
+                            output.checked_transactions += 1
+                        }
+                        else if (telco_transactions[4] === "USD") {
+                            output.global_amount_usd_credit += parseFloat(telco_transactions[2])
+                            output.checked_transactions += 1
+                        }
+                    }
+                }
+                else {
+                    output.not_matched_transactions.push(" ")
+                    output.not_matched_transactions.push(telco_transactions)
+                    output.not_matched_transactions.push(transactions)
+                }
+            }
+            else{
+                output.not_matched_transactions.push(" ")
+                output.not_founded_transactions.push(telco_transactions)
+            }
+        });
+    }
+}
+
+let computeOrange = (output, switchData, telco, opsType) => {
+    if (opsType === "1") {
+        switchData.forEach(transactions => {
+            let telco_transac = telco.find(el => el[0] === transactions[0])
+            if (telco_transac[2] === transactions[1] && telco_transac[4] === transactions[4] &&
+                telco_transac[3] === transactions[2] && telco_transac[1] === transactions[3]) {
+                if (transactions[1] === "debit") {
+                    if (transactions[4] === "CDF") {
+                        output.global_amount_cdf_debit += parseFloat(transactions[2])
+                        output.checked_transactions += 1
+                    }
+                    else if (transactions[4] === "USD") {
+                        output.global_amount_usd_debit += parseFloat(transactions[2])
+                        output.checked_transactions += 1
+                    }
+                }
+                else if (transactions[1] === "credit") {
+                    if (transactions[4] === "CDF") {
+                        output.global_amount_cdf_credit += parseFloat(transactions[2])
+                        output.checked_transactions += 1
+                    }
+                    else if (transactions[4] === "USD") {
+                        output.global_amount_usd_credit += parseFloat(transactions[2])
+                        output.checked_transactions += 1
+                    }
+                }
+            }
+            else {
+                output.not_matched_transactions.push(" ")
+                output.not_matched_transactions.push(transactions)
+                output.not_matched_transactions.push(telco_transac)
+            }
+        });
+    }
+    else if (opsType === "2") {
+        telco.forEach(telco_transactions => {
+            let transactions = switchData.find(el => el[0] === telco_transactions[0])
+            if (telco_transactions[2] === transactions[1] && telco_transactions[4] === transactions[4] &&
+                telco_transactions[3] === transactions[2] && telco_transactions[1] === transactions[3]) {
+                if (telco_transactions[1] === "debit") {
+                    if (telco_transactions[4] === "CDF") {
+                        output.global_amount_cdf_debit += parseFloat(telco_transactions[2])
+                        output.checked_transactions += 1
+                    }
+                    else if (telco_transactions[4] === "USD") {
+                        output.global_amount_usd_debit += parseFloat(telco_transactions[2])
+                        output.checked_transactions += 1
+                    }
+                }
+                else if (telco_transactions[1] === "credit") {
+                    if (telco_transactions[4] === "CDF") {
+                        output.global_amount_cdf_credit += parseFloat(telco_transactions[2])
+                        output.checked_transactions += 1
+                    }
+                    else if (telco_transactions[4] === "USD") {
+                        output.global_amount_usd_credit += parseFloat(telco_transactions[2])
+                        output.checked_transactions += 1
+                    }
+                }
+            }
+            else {
+                output.not_matched_transactions.push(" ")
+                output.not_matched_transactions.push(telco_transactions)
+                output.not_matched_transactions.push(transactions)
+            }
+        });
+    }
 }
 
 
-let calculateOutput = (outputformat, operationType) => {
-    /**
-     * operationType should be (paydrc_to_telco or telco_to_paydrc)
-     * if output == "excel" then return and excel file
-     * else if output == "csv" then return csv file
-     * this returned file should be automatically download by the user
-     */
-    console.log(outputformat, operationType);
-}
-
-export { processCsv, calculateOutput, processExcel};
+export { calculateOutput };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,132 @@
 <template>
-  <div class="row mx-auto mb-2">
-    <div class="col">Instruction with data file format (in excel and csv)</div>
+  <div class="row mx-auto mb-5">
+    <div class="col">
+      <select
+        @change="update"
+        v-model="telcoid"
+        class="form-select"
+        aria-label="Default select example"
+      >
+        <option selected>Choose a telco operator</option>
+        <option value="1">Airtel</option>
+        <option value="2">Vodacom</option>
+        <option value="3">Orange</option>
+      </select>
+    </div>
   </div>
   <div class="row mx-auto mb-2">
-    <div class="col">process page give user choice between csv and excel data to insert</div>
+    <div class="col">
+      <div class="mb-3">
+        <label for="formFile" class="form-label">Switch File</label>
+        <input
+          @change="upload('Switch', $event)"
+          class="form-control"
+          type="file"
+          id="paydrc"
+          accept=".xlsx, .xls, application/msexcel"
+        />
+      </div>
+    </div>
+    <div class="col">
+      <div class="mb-3">
+        <label for="formFile" class="form-label">{{ telco_name }} File</label>
+        <input
+          @change="upload('Telco', $event)"
+          class="form-control"
+          type="file"
+          id="telco"
+          accept=".xlsx, .xls, application/msexcel"
+        />
+      </div>
+    </div>
   </div>
   <div class="row mx-auto">
-    <div class="col">output in csv or excel depends on user needs</div>
+    <div class="col">
+      <div class="alert alert-success" role="alert">Report produced succesfully, please download</div>
+    </div>
+  </div>
+  <div class="row mx-auto mb-3">
+    <div class="col-3">
+      <div class="form-check">
+        <input class="form-check-input" value="1" @change="updateRadio" type="radio" name="ops"/>
+        <label class="form-check-label">Check Switch to Telco</label>
+      </div>
+    </div>
+    <div class="col-3">
+      <div class="form-check">
+        <input class="form-check-input" value="2" @change="updateRadio" type="radio" name="ops" />
+        <label class="form-check-label">Check Telco to Switch</label>
+      </div>
+    </div>
+  </div>
+  <div class="row mx-auto">
+    <div class="d-grid gap-2">
+      <button class="btn btn-primary" type="button" @click="produce">
+        <div class="spinner-border spinner-border-sm" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>&nbsp; Produce Report
+      </button>
+      <button class="btn btn-success" type="button" @click="download">
+        <svg-icon type="mdi" :path="mdiDownload"></svg-icon>Download Report
+      </button>
+    </div>
   </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
 
-// create user input variables that contains user data for paydrc and telco (2 variables)
+import SvgIcon from "@jamescoyle/vue-icon";
+import { mdiDownload } from "@mdi/js";
+import readXlsxFile from 'read-excel-file';
+import { calculateOutput } from "../utils/utilities";
 
-// check if user choose to user csv or excel (1 variable)
+let ops_type = ref(0);
+let telcoid = ref(0);
+let telco_name = ref("#TelcoOperator");
+let switch_file = [];
+let telco_file = [];
+let report_data = [];
 
-// process with the right method (processExcel or processCsv) both file (paydrc ans telco)
+function upload(file, event) {
+  let temp = event.target.files[0];
+  readXlsxFile(temp).then((rows) => {
+    console.log(rows);
+    if (file === 'Telco') {
+      telco_file = rows;
+    }
+    else {
+      switch_file = rows;
+    }
+  })
+}
 
-// return the processed data to  variables (2 variables)
+function updateRadio(event){
+  ops_type.value = event.target.value;
+}
 
-// choose which operations is require (check paydrc to telco or telco to paydrc) and the output format (csv or excel) (2 variables)
+function update() {
+  if (telcoid.value === "1") {
+    telco_name.value = "Airtel"
+  }
+  else if (telcoid.value === "2") {
+    telco_name.value = "Vodacom"
+  }
+  else if (telcoid.value === "3") {
+    telco_name.value = "Orange"
+  }
+  else {
+    telco_name.value = "#TelcoOperator"
+  }
+}
 
-// calculate with calculateOutput (1 variable)
+function produce() {
+  report_data = calculateOutput(switch_file, telco_file, telcoid.value, ops_type.value)
+}
 
-// download te output file in the user browser
-
-// finished
+function download() {
+  console.log(report_data);
+}
 
 
 </script>
